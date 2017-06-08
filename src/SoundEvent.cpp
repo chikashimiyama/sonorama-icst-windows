c96345953d5ee8779c8e656d42ca03bbb287eeea
Chikashi Miyama <chikashimiyama@gmail.com>
2017-06-08 18:00:15 +0200

    removed dynamic allocation of voice

---
+++ src/SoundEvent.cpp
@@ -0,0 +1,9 @@
+//
+//  SoundEvent.cpp
+//  sonorama
+//
+//  Created by Chikashi Miyama on 07.06.17.
+//
+//
+
+#include "SoundEvent.hpp"
---
+++ src/SoundEvent.hpp
@@ -0,0 +1,14 @@
+//
+//  SoundEvent.hpp
+//  sonorama
+//
+//  Created by Chikashi Miyama on 07.06.17.
+//
+//
+
+#ifndef SoundEvent_hpp
+#define SoundEvent_hpp
+
+#include <stdio.h>
+
+#endif /* SoundEvent_hpp */
